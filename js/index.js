6c2fce3b34efd33fee20ce1d67986eaf7a61ec9d
Youngjooham <young.joo1@hotmail.com>
2022-12-03 22:05:46 +0100

    EDIT

---
+++ js/index.js
@@ -0,0 +1,31 @@
+const url= "http://localhost/worldkitchen/wp-json/wp/v2/posts?per_page=3";
+
+// To show maximum 15 pages 
+//const url= "http://localhost/worldkitchen/wp-json/wp/v2/posts?per_page=15"; 
+const postContainer = document.querySelector(".latest-posts");
+
+async function getPosts(){
+    try{
+        const response = await fetch(url);
+        const getResults = await response.json();
+        createHTML(getResults);
+    }
+
+    catch(error){
+        console.log(error);
+    }
+}
+
+getPosts();
+
+function createHTML(posts){
+    posts.forEach(function(post){
+        console.log(post)
+        postContainer.innerHTML += 
+        `<div class="post">
+            <img src="${post.better_featured_image.source_url}" alt="${post.content.rendered}">
+            <h2>${post.title.rendered}</h2>
+            ${post.excerpt.rendered}
+        </div>`;
+    })
+}
